22c93aa90100b0909ff801b9bfdd6159ce083238
Rachel Wyatt <rw126@evansville.edu>
2019-03-01 00:52:08 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,5 +1,6 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
 
   def self.catamaran_operators
     self.joins(:classifications).where("classification.name = 'Catamaran'")
